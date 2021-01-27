e944004bfcc9dbb7ed6a445767653c6f17438765
Camila Martinez Bedoya <camilamarb98@gmail.com>
2021-01-27 03:55:06 -0500

    added text to binary converter

--- scripts/binaryToDecimal.js
+++ scripts/binaryToDecimal.js
@@ -69,20 +69,23 @@ function acceptOnlyLetters(e) {
 
 function convertTxtToBinary() {
   var txt = document.getElementById("txtToBinary").value;
-  var ascii = txt.charCodeAt(0);
-  let array = [0, 64, 32, 16, 8, 4, 2, 1];
   let binaryArray = [];
-  let sum = 0;
+  for (let char = 0; char < txt.length; char++) {
+    var ascii = txt.charCodeAt(char);
+    let array = [0, 64, 32, 16, 8, 4, 2, 1];
+    let sum = 0;
 
-  for (let i = 0; i < array.length; i++) {
-    if (sum < ascii) {
+    for (let i = 0; i < array.length; i++) {
       sum += array[i];
-      if (sum > ascii || array[i] == 0) {
+      if (sum > ascii) {
         sum -= array[i];
+        binaryArray.push(0);
+      } else if (array[i] == 0) {
         binaryArray.push(0);
       } else binaryArray.push(1);
     }
   }
+  console.log(binaryArray.join(""));
   document.getElementById("txtbinary").innerHTML = binaryArray.join("");
   var binaryContainer = document.getElementById("txtBinary_container");
   binaryContainer.style.display = "block";

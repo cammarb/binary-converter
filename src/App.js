43961249499476a743ced7a95ef6cc0fd154fe5b
Camila Martinez <camilamarb98@gmail.com>
2022-05-18 17:27:48 -0500

    add Forms component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Forms from "./components/Forms";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Forms />
     </>
   );
 }
---
+++ src/components/Forms.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Forms extends Component {
+    render() {
+        return (
+            <div>Forms</div>
+        )
+    }
+}

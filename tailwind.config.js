588a7b3e164e459118d7e5302d5e69a8c2f3bd4f
Camila Martinez <camilamarb98@gmail.com>
2022-05-18 17:20:07 -0500

    add font defaults to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Roboto', 'ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['Consolas', 'Roboto Mono', 'Ubuntu Mono', 'SFMono-Regular']
+    }
   },
   plugins: [],
 }

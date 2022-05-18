ecbd6953e260316ba93a27b77cf2d772a89afa9b
Camila Martinez <camilamarb98@gmail.com>
2022-05-18 13:20:03 -0500

    remove old files

--- scripts/orderingArrays.js
+++
@@ -1,7 +0,0 @@
-function reoderArray(array) {
-  var finalArray = [];
-  for (let i = 0; i < array.length; i++) {
-    finalArray.push(array[array.length - i - 1]);
-  }
-  return finalArray.join("");
-}

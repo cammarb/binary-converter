44bf105e422e7865cf966c3ae32fe0db1b25d306
Camila Martinez <camilamarb98@gmail.com>
2022-05-19 00:08:49 -0500

    update button styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = ({ children }) => {
     return (
-        <button className='px-5 py-2 text-white font-medium bg-green-500 rounded-md hover:bg-green-600'>
+        <button className='px-5 py-2 text-white font-medium rounded-md bg-green-500 hover:bg-green-600'>
             {children}
         </button>
     )

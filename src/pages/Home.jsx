c4ea45349232adc3174f9a322f5933c756e34172
Camila Martinez <camilamarb98@gmail.com>
2022-05-24 21:05:36 -0500

    cleanup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import { Forms } from "../components/Forms";
 
 export const Home = () => {
     return (
-        <div>
+        <>
             <Forms title={"Decimal to Binary"} id={"decimal"} />
             <Forms title={"Text to Binary"} id={"text"} />
-        </div>
+        </>
     )
 }

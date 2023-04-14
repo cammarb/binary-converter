9d5004ce5452af5698d2b79e89b06ad384d9a988
Camila Martinez <camilamarb98@gmail.com>
2023-04-14 09:51:55 +0200

    Add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
 import { NavLink } from "react-router-dom";
+import { MdMenu, MdOutlineClose } from "react-icons/md";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <nav className="w-full flex justify-between">
+      <nav className="w-full p-3 border-b-2 flex justify-between items-center">
         <NavLink to={"/"}>Binary Converter</NavLink>
-        <NavLink to={"/Learn"}>Learn</NavLink>
+        <div className="flex gap-4 items-center">
+          <NavLink to={"/learn"}>Learn</NavLink>
+          <button
+            className="p-2 bg-slate-200 rounded-md"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            {isOpen ? <MdOutlineClose size={20} /> : <MdMenu size={20} />}
+          </button>
+        </div>
       </nav>
     </>
   );
